b434c728186372fc3074d173ad48e9e6e6948034
Ramirez Vargas, Luis Manuel <LRamirezVargas@luxoft.com>
2018-08-19 22:09:48 -0500

    Initial commit

---
+++ ConcurrencyWorkshop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ConcurrencyWorkshop
+//
+//  Created by LRamirezVargas on 8/19/18.
+//  Copyright © 2018 luismdev. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
